c0f4a6d638116cd419a217053bdc5189c6338243
Aaron Sarazan <aaron@sarazan.net>
2019-04-10 17:56:21 -0700

    first crack at controllers too

---
+++ Script/Lifecycle/Controller.ts
@@ -0,0 +1,42 @@
+import {RegisterableComponent} from "../Ui/Lifecycle/RegisterableComponent";
+
+export default abstract class Controller extends RegisterableComponent {
+
+    private static map = new Map<Function, Controller>()
+    public static get<T extends Controller>(ctor: Function): T|null {
+        return this.map.get(ctor) as T
+    }
+
+    protected onControllerAwake() {}
+
+    protected onLoad(): void {
+        super.onLoad();
+        this.setInstance()
+        this.onControllerAwake()
+    }
+
+    protected onEnable(): void {
+        super.onEnable()
+        this.setInstance()
+    }
+
+    onRegister() {
+        super.onRegister();
+        this.setInstance()
+    }
+
+    didUnregister() {
+        super.didUnregister();
+        this.clearInstance()
+    }
+
+    private setInstance() {
+        Controller.map.set(this.constructor, this)
+    }
+
+    private clearInstance() {
+        if (Controller.get(this.constructor) == this)
+            Controller.map.delete(this.constructor)
+    }
+
+}
