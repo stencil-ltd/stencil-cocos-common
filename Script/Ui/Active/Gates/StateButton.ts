8ca6096233c4b31a6827faf7817ecf8af3f329e8
Aaron Sarazan <aaron@sarazan.net>
2019-04-15 14:52:53 -0700

    State Button

---
+++ Script/Ui/Active/Gates/StateButton.ts
@@ -0,0 +1,18 @@
+import property = cc._decorator.property;
+import StateMachine from "../../../State/StateMachine";
+
+export abstract class StateButton<T> extends cc.Component {
+
+    @property()
+    public key: string = ''
+
+    public abstract state: T
+
+    protected onLoad(): void {
+        this.node.on('click', this.onClick, this)
+    }
+
+    private onClick() {
+        StateMachine.get(this.key).requestState(this.state)
+    }
+}
