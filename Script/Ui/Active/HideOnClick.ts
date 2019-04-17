9953e3d80b1e6f2a98a11a63c84d69197e0df07e
Aaron Sarazan <aaron@sarazan.net>
2019-04-17 11:20:45 -0700

    hide on click

---
+++ Script/Ui/Active/HideOnClick.ts
@@ -0,0 +1,23 @@
+import menu = cc._decorator.menu;
+import requireComponent = cc._decorator.requireComponent;
+import Button = cc.Button;
+import property = cc._decorator.property;
+
+const {ccclass} = cc._decorator;
+
+@ccclass
+@menu("Stencil/Ui/HideOnClick")
+export default class HideOnClick extends cc.Component {
+
+    @property(cc.Node)
+    target: cc.Node = null
+
+    protected onLoad(): void {
+        if (!this.target) this.target = this.node
+        this.node.on('click', this.onClick, this)
+    }
+
+    private onClick() {
+        this.target.active = false
+    }
+}
