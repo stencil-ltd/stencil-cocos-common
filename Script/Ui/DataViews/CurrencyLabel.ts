4abaf2ca9d19dfad7196d4f0c5cca927f9bbe727
Aaron Sarazan <aaron@sarazan.net>
2019-05-07 09:07:40 -0700

    lerp amount

--- Script/Ui/DataViews/CurrencyLabel.ts
+++ Script/Ui/DataViews/CurrencyLabel.ts
@@ -15,17 +15,45 @@ export default class CurrencyLabel extends CurrencyView {
     public prefix: string = ''
 
     private _label: Label
+    private _amount: number = null
+
+    private _goal = null
 
     protected onLoad(): void {
         this._label = this.getComponent(Label)
     }
 
-    protected onChange() {
-        const amount = this.amount()
-        if (amount != null) {
-            this._label.string = `${this.prefix}${this.amount()}`
+    protected update(dt: number): void {
+        super.update(dt);
+        if (this._goal == null) return
+        const progress = Date.now() - this._goal.begin
+        const norm = Math.min(1, progress / this._goal.duration)
+        this._amount = Math.floor(cc.misc.lerp(this._goal.start, this._goal.amount, norm))
+        if (this._goal.amount === this._amount) this._goal = null
+    }
+
+    protected lateUpdate(): void {
+        if (this._amount != null) {
+            this._label.string = `${this.prefix}${this._amount}`
         } else {
             this._label.string = ''
+        }
+    }
+
+    protected onChange() {
+        if (this._amount == null) {
+            this._amount = this.amount()
+        } else {
+            this._lerp(this.amount())
+        }
+    }
+
+    private _lerp(amount: number) {
+        this._goal = {
+            start: this._amount,
+            begin: Date.now(),
+            duration: 1000,
+            amount: amount
         }
     }
 }
