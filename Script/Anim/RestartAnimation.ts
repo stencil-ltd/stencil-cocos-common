4cb560b242281fbc8a7765ae1b391f78a34bdf81
Aaron Sarazan <aaron@sarazan.net>
2019-05-13 14:20:57 -0700

    restart-animation gets anim hack

--- Script/Anim/RestartAnimation.ts
+++ Script/Anim/RestartAnimation.ts
@@ -1,6 +1,7 @@
 import menu = cc._decorator.menu;
 import requireComponent = cc._decorator.requireComponent;
 import Animation = cc.Animation;
+import property = cc._decorator.property;
 
 const {ccclass} = cc._decorator;
 
@@ -9,8 +10,18 @@ const {ccclass} = cc._decorator;
 @menu("Stencil/Anim/RestartAnimation")
 export default class RestartAnimation extends cc.Component {
 
+    @property()
+    fadeInHack: boolean = false
+
     protected onEnable(): void {
         const anim = this.getComponent(Animation)
         anim.play((anim.currentClip || anim.defaultClip || anim.getClips()[0]).name, 0)
+        if (this.fadeInHack) {
+            const opacity = this.node.opacity
+            this.node.opacity = 0
+            this.scheduleOnce(() => {
+                this.node.opacity = opacity
+            }, 0.1)
+        }
     }
 }
