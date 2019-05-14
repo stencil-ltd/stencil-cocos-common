92dabe01f0f54baefb35e34f97e14f9ef205dea8
Aaron Sarazan <aaron@sarazan.net>
2019-05-14 09:44:57 -0700

    no longer need sample hacks

--- Script/Anim/StencilAnimation.ts
+++ Script/Anim/StencilAnimation.ts
@@ -12,19 +12,9 @@ export default class StencilAnimation extends cc.Component {
     @property()
     restartOnAwake: boolean = true
 
-    @property()
-    delayVisible: boolean = false
-
-    @property()
-    sampleOnEnable: boolean = true
-
-    @property()
-    sampleOnUpdate: boolean = true
-
     private _anim: cc.Animation
 
     protected onLoad(): void {
-        this._anim = this.getComponent(cc.Animation)
     }
 
     protected onEnable(): void {
@@ -32,26 +22,10 @@ export default class StencilAnimation extends cc.Component {
         if (this.restartOnAwake) {
             this._anim.play(clip.name, 0)
         }
-        if (this.sampleOnEnable) {
-            this.sample()
-        }
-        if (this.delayVisible) {
-            const opacity = this.node.opacity
-            this.node.opacity = 0
-            this.scheduleOnce(() => this.node.opacity = opacity, 0.1)
-        }
-    }
-
-    protected update(dt: number): void {
-        if (this.sampleOnUpdate) this.sample()
     }
 
     private getClip() {
         const anim = this._anim
         return (anim.currentClip || anim.defaultClip || anim.getClips()[0])
-    }
-
-    private sample() {
-        this._anim.sample(this.getClip().name)
     }
 }
