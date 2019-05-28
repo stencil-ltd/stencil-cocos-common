eb55694c20fbcf00463777d7ab421de93e224365
Aaron Sarazan <aaron@sarazan.net>
2019-05-28 11:51:09 -0700

    PromiseWrapper

---
+++ Script/Foundation/PromiseWrapper.ts
@@ -0,0 +1,58 @@
+export default class PromiseWrapper<T> {
+
+    public readonly promise: Promise<T>
+
+    private _value: T | PromiseLike<T> | null
+    private _reason: any|null
+
+    private _resolve: (value?: T | PromiseLike<T>) => void
+    private _reject: (reason?: any) => void
+
+    private _isResolved = false
+    private _isRejected = false
+
+    public get success(): boolean|null {
+        if (this._isResolved) {
+            return true
+        }
+        if (this._isRejected) {
+            return false
+        }
+        return null
+    }
+
+    public get value(): T | PromiseLike<T> | null {
+        return this._value
+    }
+
+    public get reason(): any | null {
+        return this._reason
+    }
+
+    public isFinished() {
+        return this._isResolved || this._isRejected
+    }
+
+    constructor() {
+        this.promise = new Promise<T>((resolve, reject) => {
+            this._resolve = resolve
+            this._reject = reject
+        })
+    }
+
+    public resolve(value?: T | PromiseLike<T>) {
+        if (this.isFinished()) {
+            throw `Already finished (${this._isResolved})`
+        }
+        this._value = value
+        this._resolve(value)
+    }
+
+    public reject(reason?: any) {
+        if (this.isFinished()) {
+            throw `Already finished (${this._isResolved})`
+        }
+        this._reason = reason
+        this._reject(reason)
+    }
+}
