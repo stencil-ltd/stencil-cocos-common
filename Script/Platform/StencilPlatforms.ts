091dfaa0568e06d3bea100cee0993f0e5b60ab7b
Aaron Sarazan <aaron@sarazan.net>
2019-05-29 16:40:04 -0700

    platform helper

--- Script/Platform/StencilPlatforms.ts
+++ Script/Platform/StencilPlatforms.ts
@@ -1,4 +1,15 @@
+import {Platform} from "../Ui/PlatformVisible";
+import PlatformSwitch from "./PlatformSwitch";
+
 export default class StencilPlatforms {
+
+    // @ts-ignore
+    public static platformString: "ios" | "android" | "simulator" =
+        new PlatformSwitch<string>()
+            .withDefault("simulator")
+            .withIos("ios")
+            .withAndroid("android")
+            .value()
 
     public static isAndroid() {
         return cc.sys.platform == cc.sys.ANDROID
