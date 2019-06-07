38adeee1dc02069c54e512edb3b8c4c9e413eae7
Aaron Sarazan <aaron@sarazan.net>
2019-06-07 11:07:57 -0700

    smarten init script

--- bin/init.sh
+++ bin/init.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-PROJECT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../../.."
-echo "dir = ${PROJECT}"
+OLD_DIR=`pwd`
 
+# Find root dir for enclosing project. 
+# This relies on the stencil-cocos-common module being placed in $PROJECT/assets/
+PROJECT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../../.."
 cd $PROJECT
+
+# Initialize npm if no package data exists
+if [ ! -f $PROJECT/package.json ]; then
 npm init -y
+fi
+
+# Install required libraries for common layer
 npm install moment
+
+# Navigate back to where we started
+cd $OLD_DIR
