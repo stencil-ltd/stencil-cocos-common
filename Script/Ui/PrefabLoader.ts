e6dc353d5b183322f3cbbd239aa9adc95295fa38
Aaron Sarazan <aaron@sarazan.net>
2019-06-13 17:10:38 -0700

    prefabloader

---
+++ Script/Ui/PrefabLoader.ts
@@ -0,0 +1,19 @@
+import menu = cc._decorator.menu;
+import property = cc._decorator.property;
+import Prefab = cc.Prefab;
+import {RegisterableComponent} from "./Lifecycle/RegisterableComponent";
+
+const {ccclass} = cc._decorator;
+
+@ccclass
+@menu("Stencil/Ui/PrefabLoader")
+export default class PrefabLoader extends RegisterableComponent {
+
+    @property(Prefab)
+    prefab: Prefab
+
+    onRegister() {
+        super.onRegister();
+        this.node.addChild(cc.instantiate(this.prefab))
+    }
+}
