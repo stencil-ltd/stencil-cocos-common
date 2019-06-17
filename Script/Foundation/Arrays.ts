7bf9b5f8303eddd88f35291eff325095d9c29762
Aaron Sarazan <aaron@sarazan.net>
2019-06-17 13:11:36 -0700

    fix arrays

--- Script/Foundation/Arrays.ts
+++ Script/Foundation/Arrays.ts
@@ -1,6 +1,7 @@
 interface Array<T> {
     last(): T
     lastOrNull(): T|null
+    first<R>(fn: (value: T) => R|null|undefined): R|null
 }
 
 Array.prototype.last = function () {
@@ -10,4 +11,12 @@ Array.prototype.last = function () {
 Array.prototype.lastOrNull = function() {
     if (this.length == 0) return null
     return this.last()
+}
+
+Array.prototype.first = function<R> (fn: (value) => R|null|undefined): R|null {
+    for (let x = 0; x < this.length; ++x) {
+        const result = fn(this[x])
+        if (result) return result
+    }
+    return null
 }
