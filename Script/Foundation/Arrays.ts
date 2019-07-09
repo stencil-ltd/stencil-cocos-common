d8a1a4079789bb7df788f4b079d45655a287201c
Aaron Sarazan <aaron@sarazan.net>
2019-07-09 11:33:03 -0700

    sentence splitting helpers

--- Script/Foundation/Arrays.ts
+++ Script/Foundation/Arrays.ts
@@ -2,6 +2,7 @@ interface Array<T> {
     last(): T
     lastOrNull(): T|null
     first<R>(fn: (value: T) => R|null|undefined): R|null
+    halves(roundUp?: boolean): T[][]
 }
 
 Array.prototype.last = function () {
@@ -19,4 +20,13 @@ Array.prototype.first = function<R> (fn: (value) => R|null|undefined): R|null {
         if (result) return result
     }
     return null
+}
+
+//https://stackoverflow.com/a/47777083
+Array.prototype.halves = function(roundUp: boolean = false) {
+    let yourArray = this
+    let halfWayThough = roundUp ? Math.ceil(yourArray.length / 2) : Math.floor(yourArray.length / 2)
+    let arrayFirstHalf = yourArray.slice(0, halfWayThough);
+    let arraySecondHalf = yourArray.slice(halfWayThough, yourArray.length);
+    return [arrayFirstHalf, arraySecondHalf]
 }
--- Script/Foundation/StencilText.ts
+++ Script/Foundation/StencilText.ts
@@ -18,4 +18,24 @@ export default class StencilText {
         return indices;
     }
 
+    public static splitSentences(text: string, maxLength: number): string[] {
+        if (!text) return []
+        if (text.length <= maxLength) return [text]
+        try {
+            const sentences: string[] = text.match(/[^\.!\?]+[\.!\?]+/g);
+            let split = sentences.halves(true)
+            const first = split[0].join('')
+            const second = split[1].join('')
+            const final = [first, second]
+            const retval: string[] = []
+            final.forEach(value => {
+                retval.push(...this.splitSentences(value, maxLength))
+            })
+            return retval.map(value => value.trim())
+        } catch (e) {
+            console.error(e)
+            return [text]
+        }
+    }
+
 }
